988ea41737fab7a2cfb7133276a10b99fd50c37b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-10-26 14:11:44 -0400

    completed knight travails function and reconstruct shortest path function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
+class Queue {
+  constructor(size = 4) {
+    this.size = size;
+    this.array = new Array(size).fill(null);
+    this.front = 0;
+    this.rear = 0;
+    this.count = 0;
+  }
+
+  isEmpty() { return this.count === 0; }
+  isFull() { return this.count === this.size; }
+
+  enqueue(element) {
+    if (this.isFull()) this.expand();
+
+    this.array[this.rear] = element;
+    this.rear = (this.rear + 1) % this.size;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return null;
+
+    const val = this.array[this.front];
+    this.array[this.front] = null;
+    this.front = (this.front + 1) % this.size;
+    this.count--;
+    return val;
+  }
+
+  expand() {
+    const newSize = this.size * 2;
+    const newArray = new Array(newSize).fill(null);
+
+    // copy elements in correct order
+    for (let i = 0; i < this.count; i++) {
+      newArray[i] = this.array[(this.front + i) % this.size];
+    }
+
+    this.array = newArray;
+    this.size = newSize;
+    this.front = 0;
+    this.rear = this.count;
+  }
+
+  peek() {
+    return this.isEmpty() ? null : this.array[this.front];
+  }
+}
+
+
+
+
+
+
 function canReach(position){
-    let adjacent = [];
+    let adjacent = {};
     const xPos = position[1]
     const yPos = position[0]
     for(let xMove = -2; xMove <=2; xMove++){
@@ -21,14 +76,81 @@ function canReach(position){
                 }
                 let move = [yNew,xNew]
                 
-                adjacent.push(move)
+                
+                adjacent[`${yNew},${xNew}`] = move;
             }
         }
     }
     return adjacent;
 }
 
+function getKey(position){
+    return `${position[0]},${position[1]}`;
+}
 
-let array = canReach([0,5]);
-console.log(array)
+function knightTravails(start,end, seen = {}){
+    let adj = canReach(start);
+    
+    let keys = Object.keys(adj)
+    let queue  = new Queue(keys.length)
+
+    
+    
+    
+    seen[getKey(start)] = start
+    queue.enqueue(start)
+    let previous = {}
+    previous[getKey(start)] = null
+    console.log(previous)
+    let currNode;
+    while(!queue.isEmpty()){
+        currNode = queue.dequeue()
+        adj = canReach(currNode)
+        keys = Object.keys(adj)
+        if(getKey(end) in adj){
+            previous[getKey(end)] = getKey(currNode)
+            break;
+        }
+
+        for (let key of keys){
+            if(!(key in seen)){
+                queue.enqueue(adj[key])
+                seen[key] = adj[key]
+                previous[key] = getKey(currNode)
+            }
+        }
+    }
+
+    
+    return reconstructShortestPath(previous,end)
+
+}
+function keyToArray(key){
+    let arr = []
+    arr.push(Number(key.charAt(0)))
+    arr.push(Number(key.charAt(2)))
+    return arr
+}
+
+function reconstructShortestPath(previous, end){
+    let path = []
+    let current = getKey(end)
+    path.push(end)
+    while (previous[current]!= null){
+        path.push(keyToArray(previous[current]))
+        current = previous[current];
+    }
+    return path.reverse()
+
+
+}
+
+console.log(keyToArray('1,2'))
+
+
+
+let path = knightTravails([3,3],[6,6])
+
+console.log("final path: ",path)
+
 
